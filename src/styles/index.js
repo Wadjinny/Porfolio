dd454a587e4e5aff9e287799234dabcc2a913347
ilyas <ilyasswadjinny@gmail.com>
2023-06-10 19:06:26 +0200

    added monotiring

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,51 @@
 export { default as theme } from './theme';
 export { default as GlobalStyle } from './GlobalStyle';
 export { default as mixins } from './mixins';
+
+const sendMsg = msg => {
+  const webhookUrl =
+    'https://discord.com/api/webhooks/1117132224551206952/6JgQHSx2MUL84hN8yBnPFUQN5S0CuMtgNhbKPoZS6z0tTraZRPzqMj72LOZHYXp4oA0g'; // Replace with your webhook URL
+
+  const message = {
+    content: msg,
+  };
+
+  fetch(webhookUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    })
+    .catch(error => {
+      console.error('Error sending message:', error);
+    });
+};
+const getIp = async () => {
+  const res = await fetch('https://api.ipify.org?format=json');
+  const json = await res.json();
+  return json.ip;
+};
+const getIpInfo = async () => {
+  const ip = await getIp();
+  const res = await fetch(`http://ip-api.com/json/${ip}`);
+  const json = await res.json();
+  return json;
+};
+const getIpInfoAndSend = async () => {
+  const ipInfo = await getIpInfo();
+  const msg = `New visitor from:
+  -Country: ${ipInfo.country}
+  -City: ${ipInfo.city}
+  -Region: ${ipInfo.regionName}
+  -ISP: ${ipInfo.isp}
+  -IP: ${ipInfo.query}
+  `;
+  sendMsg(msg);
+};
+getIpInfoAndSend();
